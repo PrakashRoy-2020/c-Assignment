5bcabcdc7870a7264f9748d24fd926b61612b88a
Prakash Roy <prakash.roy@zensar.com>
2020-08-03 17:02:20 +0530

    c#assignment

---
+++ Assignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assignment;
+
+namespace Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PropClass obj = new PropClass();
+            obj.Date = Convert.ToInt32(Console.ReadLine());
+            obj.Month = Convert.ToInt32(Console.ReadLine());
+            obj.Year = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0}/{1}/{2}",obj.Date,obj.Month,obj.Year);
+
+        }
+    }
+}
---
+++ Assignment/PropClass.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    class PropClass
+    {
+        private int date;
+        private int month;
+        protected internal int year;
+
+        public int Date
+        {
+            get
+            {
+                return date;
+            }
+            set
+            {
+                if (value >= 1 && value <= 31)
+                {
+                    date = value;
+                }
+                else
+                {
+                    
+                    Console.WriteLine("in b/w 1 to 31");
+                }
+            }
+        }
+
+        public int Month
+        {
+            get
+            {
+                return month;
+            }
+            set
+            {
+                if ((value >= 1) && (value <= 12))
+                {
+                    month = value;
+                }
+                else
+                {
+                    
+                    Console.WriteLine("in b/w 1 to 12");
+                }
+            }
+        }
+        public int Year
+        {
+            get
+            {
+                return year;
+            }
+            set
+            {
+                if ((value >= 2018) && (value <= 2020))
+                {
+                    year = value;
+                }
+                else
+                {
+                    
+                    Console.WriteLine("in b/w 2018 to 2020");
+                }
+            }
+        }
+    }
+    class AssignmentLib
+    {
+       
+    }
+    //class PropClass
+    //{
+    //}
+}
